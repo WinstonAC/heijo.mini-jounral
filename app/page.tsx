058e85d7b061a0f90509404ddf223025b7f17894
WinstonAC <billy.campbell17@gmail.com>
2025-12-03 14:25:02 +0100

    chore: redirect root to /login

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
-'use client';
+import { redirect } from "next/navigation";
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { trace } from '@/lib/diagnostics/routeTrace';
-
-export default function HomePage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    trace('HomePage mounted - redirecting to login (intro removed)');
-    router.push('/login');
-  }, [router]);
-
-  return null;
+export default function Home() {
+  redirect("/login");
 }
 
 
